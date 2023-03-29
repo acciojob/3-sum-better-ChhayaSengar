e9ba409b4bd3a0c0f2f01001739a2cbc8d5c9339
Chhaya <117908142+ChhayaSengar@users.noreply.github.com>
2023-03-29 15:15:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-function threeSum(arr, target) {
-// write your code here
-  let min =Number.MAX_VALUE;
-	let ans=0;
-	for(let i=0; i<arr.length;i++){
-		for(let j=i+1; j<arr.length; j++){
-			for (let k = j+1; k < arr.length; k++){
-				let sum = arr[i] + arr[j] +arr[k];
-				if(Math.abs(sum-target)<=min)
-					min =Math.abs(sum-target);
-				  ans= sum;
-			    }
-        }
-     }
-	return ans;
-}
+// function threeSum(arr, target) {
+// // write your code here
+//   let min =Number.MAX_VALUE;
+// 	let ans=0;
+// 	for(let i=0; i<arr.length;i++){
+// 		for(let j=i+1; j<arr.length; j++){
+// 			for (let k = j+1; k < arr.length; k++){
+// 				let sum = arr[i] + arr[j] +arr[k];
+// 				if(Math.abs(sum-target)<=min)
+// 					min =Math.abs(sum-target);
+// 				  ans= sum;
+// 			    }
+//         }
+//      }
+// 	return ans;
+// }
 
-module.exports = threeSum;
+// module.exports = threeSum;
+var threeSumClosest = function (nums, target) {
+    // Sort the array
+    nums.sort((a, b) => a - b);
+    // Length of the array
+    const n = nums.length;
+    // Result
+    let closest = nums[0] + nums[1] + nums[n - 1];
+    // Loop for each element of the array
+    for (let i = 0; i < n - 2; i++) {
+        // Left and right pointers
+        let j = i + 1;
+        let k = n - 1;
+        // Loop for all other pairs
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (sum <= target) {
+                j++;
+            } else {
+                k--;
+            }
+            if (Math.abs(closest - target) > Math.abs(sum - target)) {
+                closest = sum;
+            }
+        }
+    }
+    return closest;
+};
